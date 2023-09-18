b8da2285a676954097c7dca80edf0a0b2ebf51e9
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-18 16:32:13 -0300

    Test API endpoint with fetch inside a useEffect hook

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,10 @@
 class Api::GreetingsController < ApplicationController
   def random_greeting
     random_message = Message.find(rand(1..5))
-    render json: { greeting: random_message }
+    if random_message
+      render json: { greeting: random_message.content }
+    else
+      render json: { error: 'No greetings available' }, status: :not_found
+    end
   end
 end
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Greeting() {
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/greetings/random_greeting')
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.greeting);
+      })
+  }, [])
+
   return (
-      <h1>This is a random greeting</h1>
+      <h1>This is a random greeting: {message}</h1>
     );
 }
 
