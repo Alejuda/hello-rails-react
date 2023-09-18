42e42c26670c653ff2fc91550b14468096eb67c2
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-18 16:34:25 -0300

    Fix linter errors

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,10 +1,14 @@
-class Api::GreetingsController < ApplicationController
-  def random_greeting
-    random_message = Message.find(rand(1..5))
-    if random_message
-      render json: { greeting: random_message.content }
-    else
-      render json: { error: 'No greetings available' }, status: :not_found
+# frozen_string_literal: true
+
+module Api
+  class GreetingsController < ApplicationController
+    def random_greeting
+      random_message = Message.find(rand(1..5))
+      if random_message
+        render json: { greeting: random_message.content }
+      else
+        render json: { error: 'No greetings available' }, status: :not_found
+      end
     end
   end
 end
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,20 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function Greeting() {
-
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch('/api/greetings/random_greeting')
+    fetch("/api/greetings/random_greeting")
       .then((response) => response.json())
       .then((data) => {
         setMessage(data.greeting);
-      })
-  }, [])
+      });
+  }, []);
 
-  return (
-      <h1>This is a random greeting: {message}</h1>
-    );
+  return <h1>This is a random greeting: {message}</h1>;
 }
 
 export default Greeting;
