c37cbec4ec67964ee79b839a20dd715bd0850785
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-18 20:40:35 -0300

    Fix reloading on a react route error

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import RandomGreeting from './routes/RandomGreeting';
 import Layout from './components/Layout';
